eaea0ea620c4d14900d6b054373b90c7b43c0c36
Nauman Zubair <nauman@csms.ae>
2019-05-09 22:53:47 +0400

    Added module that provides injection

--- app/src/main/java/com/csms/simpledagger2/MainActivity.kt
+++ app/src/main/java/com/csms/simpledagger2/MainActivity.kt
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import dagger.Component
 import dagger.Module
+import dagger.Provides
 import kotlinx.android.synthetic.main.activity_main.*
 import javax.inject.Inject
 
@@ -14,16 +15,24 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         DaggerMainActivity_InfoComponent.create().inject(this)
+
         textView.text = info.text
     }
 
-    class Info @Inject constructor(){
-        val text = "I have some info"
-    }
+    class Info (var text : String)
 
-    @Component
+    @Component(modules = [InfoModule::class])
     interface InfoComponent {
         fun inject(activity: MainActivity)
+    }
+
+    @Module
+    class InfoModule {
+        @Provides
+        fun provideInfo() : Info {
+            return Info("Get info through constructor")
+        }
     }
 }
